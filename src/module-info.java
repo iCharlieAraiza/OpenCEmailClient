0c9ae8d1412c170d36a476ffc7977f6b700c1988
iCharlieAraiza <eliptikman1234@gmail.com>
2021-07-14 15:50:56 -0500

    Init Project using Java FX

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module emailClient {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.web;
+    requires java.desktop;
+
+    opens org.main;
+}
--- src/org/main/Main.java
+++ src/org/main/Main.java
@@ -1,9 +1,31 @@
 package org.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button button = new Button("Click!");
+        button.setOnAction(e -> {
+            System.out.println("Button clicked!");
+        });
+
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(button);
+
+        Scene scene = new Scene(stackPane, 300, 250);
+        stage.setScene(scene);
+
+        stage.show();
+
     }
 }
